8f1df7bedb00ce5c712f2944c7fab08f87c085ae
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-06-25 15:55:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,138 +1,107 @@
-// Helper function to generate a random 16-byte access token
-function generateAccessToken() {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let token = '';
-    for (let i = 0; i < 16; i++) {
-      token += chars.charAt(Math.floor(Math.random() * chars.length));
+document.addEventListener("DOMContentLoaded", function() {
+  const fullnameInput = document.getElementById("fullname");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+  const confirmPasswordInput = document.getElementById("confirmPassword");
+  const errorMessage = document.getElementById("error-message");
+  const successMessage = document.getElementById("success-message");
+  const signupButton = document.getElementById("signup-button");
+  const logoutButton = document.getElementById("logout-button");
+
+  signupButton.addEventListener("click", function() {
+    const fullname = fullnameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+    const confirmPassword = confirmPasswordInput.value.trim();
+
+    if (fullname === "" || email === "" || password === "" || confirmPassword === "") {
+      errorMessage.textContent = "All fields are mandatory";
+      successMessage.textContent = "";
+      errorMessage.style.display = "block";
+      successMessage.style.display = "none";
+    } else if (password !== confirmPassword) {
+      errorMessage.textContent = "Passwords do not match";
+      successMessage.textContent = "";
+      errorMessage.style.display = "block";
+      successMessage.style.display = "none";
+    } else {
+      const accessToken = generateAccessToken(); // Function to generate a random access token
+      
+      // Create user object with properties and access token
+      const user = {
+        fullname: fullname,
+        email: email,
+        password: password,
+        accessToken: accessToken
+      };
+
+      // Save user state to local storage
+      localStorage.setItem("user", JSON.stringify(user));
+
+      // Show success message
+      errorMessage.textContent = "";
+      successMessage.textContent = "Successfully Signed up!";
+      errorMessage.style.display = "none";
+      successMessage.style.display = "block";
+
+      // Redirect to profile page
+      window.location.href = "./profile.html";
+    }
+  });
+
+
+ // Check if the user is authenticated
+ const userState = localStorage.getItem("user");
+ if (!userState) {
+   // If user state is not found, redirect to signup page
+   window.location.href = "./index.html";
+ } else {
+   // Parse the user state from local storage
+   const user = JSON.parse(userState);
+   // Display user details on the profile page
+   fullnameElement.textContent = user.fullname;
+   emailElement.textContent = user.email;
+ }
+
+  logoutButton.addEventListener("click", function() {
+    // Clear user state from local storage
+    localStorage.removeItem("user");
+    localStorage.removeItem("accessToken");
+    // Redirect to signup page
+    window.location.href = "./index.html";
+  });
+
+  const path = window.location.pathname;
+  const isAuthenticated = checkAuthentication();
+
+  if (path === "./profile.html") {
+    if (!isAuthenticated) {
+      window.location.href = "./index.html";
+    } else {
+      const user = JSON.parse(localStorage.getItem("user"));
+      document.getElementById("fullname-profile").textContent = user.fullname;
+      document.getElementById("email-profile").textContent = user.email;
+      document.getElementById("password-profile").textContent = user.password;
+    }
+  } else if (path === "./index.html") {
+    if (isAuthenticated) {
+      window.location.href = "./profile.html";
+    }
+  }
+
+  function generateAccessToken() {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const length = 16;
+    let token = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      token += characters.charAt(randomIndex);
     }
     return token;
   }
-  
-  // Function to save user data in local storage
-  function saveUserState(user) {
-    localStorage.setItem('user', JSON.stringify(user));
-  }
-  
-  // Function to retrieve user data from local storage
-  function getUserState() {
-    const user = localStorage.getItem('user');
-    return user ? JSON.parse(user) : null;
-  }
-  
-  // Function to clear user data from local storage
-  function clearUserState() {
-    localStorage.removeItem('user');
-  }
-  
-  // Function to redirect to a specific page
-  function redirectToPage(page) {
-    window.location.href = page;
-  }
-  
-  // Function to check if the user is authenticated
-  function isAuthenticated() {
-    const user = getUserState();
+
+  function checkAuthentication() {
+    const user = JSON.parse(localStorage.getItem("user"));
     return user && user.accessToken;
   }
-  
-  // Function to display user details on the profile page
-  function displayUserProfile() {
-    const user = getUserState();
-    const profileDetails = document.getElementById('profile-details');
-    profileDetails.innerHTML = `
-      <p><strong>Name:</strong> ${user.name}</p>
-      <p><strong>Email:</strong> ${user.email}</p>
-    `;
-  }
-  
-  // Function to handle signup form submission
-  function handleSignupFormSubmit(event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
-  
-    // Validate form inputs
-    if (!name || !email || !password || !confirmPassword) {
-      const errorMessage = document.getElementById('error-message');
-      errorMessage.textContent = 'Error: All fields are mandatory.';
-      return;
-    }
-  
-    if(password !== confirmPassword){
-      const errorMessage=document.getElementById('error-message');
-      errorMessage.textContent='Error:Passowrds do not match.'
-      return;
-    }
-
-
-    // Generate access token
-    const accessToken = generateAccessToken();
-  
-    // Save user state in local storage
-    const user = {
-      name,
-      email,
-      password,
-      accessToken
-    };
-    saveUserState(user);
-  
-// Clear form inputs
-document.getElementById('name').value = '';
-document.getElementById('email').value = '';
-document.getElementById('password').value = '';
-document.getElementById('confirm-password').value = '';
-
-// Clear error message
-const errorMessage = document.getElementById('error-message');
-errorMessage.textContent = '';
-
-
-    // Display success message and redirect to profile page
-    const successMessage = document.getElementById('success-message');
-    sucessMessage.textContent = 'Successfully Signed Up!';
-    const successMessage = document.createElement('p');
-    successMessage.classList.add('success-message');
-    successMessage.textContent = 'Signup successful! Redirecting to profile...';
-    document.getElementById('signup-form').appendChild(successMessage);
-    setTimeout(() => {
-      redirectToPage('./profile.html');
-    }, 2000);
-  }
-  
-  // Function to handle logout button click
-  function handleLogout() {
-    // Clear user state and redirect to signup page
-    clearUserState();
-    redirectToPage('./index.html');
-  }
-  
-  // Check if the user is authenticated
-  if (isAuthenticated()) {
-    // If authenticated, display profile page
-    if(window.location.pathname.includes('./index.html')){
-      redirectToPage('./profile.html');
-    }
-  } else {
-    // If not authenticated, display signup page
-    // redirectToPage('./index.html');
-    if(window.location.pathname.includes('./profile.html')){
-      redirectToPage('./profile.html');
-    }
-    displayUserProfile();
-  }
-  
-// function to display user details on the profile page
-function displayUserProfile(){
-  const user=getUserState();
-  document.getElementById('profile-name').textContent=user.name;
-  document.getElementById('profile-email').textContent=user.email;
-}
-
-
-  // Event listeners
-  document.getElementById('signup-form').addEventListener('submit', handleSignupFormSubmit);
-  document.getElementById('logout-btn').addEventListener('click', handleLogout);
-  
+});
